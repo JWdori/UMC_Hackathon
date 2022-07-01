081e233e6a77527b357516e4ceb089e5efc32a1e
sanghee <sh532@konkuk.ac.kr>
2022-07-01 18:05:42 +0900

    marker 표시 구현

--- app/src/main/java/com/abilitymap/MainActivity.java
+++ app/src/main/java/com/abilitymap/MainActivity.java
@@ -2,18 +2,24 @@ package com.abilitymap;
 
 import androidx.annotation.NonNull;
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.core.app.ActivityCompat;
 import androidx.fragment.app.FragmentManager;
 
 import android.os.Bundle;
 import android.util.Log;
 import android.widget.TextView;
 
+import com.naver.maps.geometry.LatLng;
 import com.naver.maps.map.MapFragment;
 import com.naver.maps.map.NaverMap;
 import com.naver.maps.map.OnMapReadyCallback;
+import com.naver.maps.map.overlay.Marker;
+import com.naver.maps.map.overlay.OverlayImage;
+import com.naver.maps.map.util.FusedLocationSource;
 
 public class MainActivity extends AppCompatActivity implements OnMapReadyCallback {
     TextView tvText;
+    private NaverMap mNaverMap;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
@@ -30,12 +36,20 @@ public class MainActivity extends AppCompatActivity implements OnMapReadyCallbac
 
 //        tvText = (TextView) findViewById(R.id.text);
 //        tvText.setText(HelloKt.formatMessage("Android with Kotlin"));
-
     }
 
     @Override
     public void onMapReady(@NonNull NaverMap naverMap) {
         Log.d("MainActivity", "onMapReady");
+
+        Marker marker = new Marker();
+        marker.setWidth(100);
+        marker.setHeight(100);
+        marker.setIcon(OverlayImage.fromResource(R.drawable.danger));
+        marker.setPosition(new LatLng(37.5670135, 126.9783740));
+        marker.setMap(naverMap);
+
+        //ActivityCompat.requestPermissions(this.);
 
     }
 }
