1dab95fe2d74e0d023e9c0b502ae2edc8c01c676
Dori <jaewan0114@naver.com>
2022-07-01 19:16:44 +0900

    conflict 해결

--- app/src/main/java/com/abilitymap/MainActivity.java
+++ app/src/main/java/com/abilitymap/MainActivity.java
@@ -1,20 +1,89 @@
 package com.abilitymap;
-
+import androidx.annotation.NonNull;
 import androidx.appcompat.app.AppCompatActivity;
-
-import android.os.Bundle;
+import androidx.core.app.ActivityCompat;
+import androidx.fragment.app.FragmentManager;
+import android.util.Log;
 import android.widget.TextView;
+import com.naver.maps.map.MapFragment;
+import com.naver.maps.map.NaverMap;
+import com.naver.maps.map.OnMapReadyCallback;
+import android.Manifest;
+import android.content.pm.PackageManager;
+import android.os.Bundle;
+import android.widget.Toast;
+import com.naver.maps.geometry.LatLng;
+import com.naver.maps.map.LocationTrackingMode;
+import com.naver.maps.map.MapFragment;
+import com.naver.maps.map.NaverMap;
+import com.naver.maps.map.OnMapReadyCallback;
+import com.naver.maps.map.util.FusedLocationSource;
 
-public class MainActivity extends AppCompatActivity {
-    TextView tvText;
+import java.util.ArrayList;
+import java.util.List;
+
+public class MainActivity extends AppCompatActivity implements OnMapReadyCallback {
+    private NaverMap naverMap;
+    private FusedLocationSource locationSource;
+
+
+    private static final int PERMISSIONS_REQUEST_CODE = 100;
+    private static final String[] PERMISSIONS = {
+            Manifest.permission.ACCESS_FINE_LOCATION,
+            Manifest.permission.ACCESS_COARSE_LOCATION
+    };
+
+
+    private static final int LOCATION_PERMISSION_REQUEST_CODE = 1000;
+    String[] REQUIRED_PERMISSIONS = {
+            Manifest.permission.ACCESS_FINE_LOCATION,
+            Manifest.permission.ACCESS_COARSE_LOCATION
+    };
+
+
+    List<LatLng> latLngList = new ArrayList<>();
+
     @Override
-    protected void onCreate(Bundle savedInstanceState) {
+    protected void onCreate(Bundle savedInstanceState){ //화면 생성과 함께 현재 위치 받아옴.
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
 
+        FragmentManager fm = getSupportFragmentManager();
+        MapFragment mapFragment = (MapFragment)fm.findFragmentById(R.id.map);
 
-//        tvText = (TextView) findViewById(R.id.text);
-//        tvText.setText(HelloKt.formatMessage("Android with Kotlin"));
+        if(mapFragment ==null ){
+            mapFragment = MapFragment.newInstance();
+            fm.beginTransaction().add(R.id.map, mapFragment).commit();
+        }
+        mapFragment.getMapAsync(this);
+
+        locationSource = new FusedLocationSource(this,LOCATION_PERMISSION_REQUEST_CODE);
 
     }
+
+
+
+    @Override
+    public void onMapReady(@NonNull NaverMap naverMap) {
+        this.naverMap = naverMap;
+        naverMap.setLocationSource(locationSource);//현재 위치 표시
+
+        ActivityCompat.requestPermissions(this, PERMISSIONS,
+                PERMISSIONS_REQUEST_CODE); //퍼미션 요청 확인
+        Log.d("MainActivity", "onMapReady");
+    }
+
+
+    @Override
+    public void onRequestPermissionsResult ( int requestCode,
+                                             @NonNull String[] permissions, @NonNull int[] grandResults) {
+        super.onRequestPermissionsResult(requestCode, permissions, grandResults);
+        if (requestCode == PERMISSIONS_REQUEST_CODE) {
+            if (grandResults.length > 0 && grandResults[0] == PackageManager.PERMISSION_GRANTED) {
+                naverMap.setLocationTrackingMode(LocationTrackingMode.Follow);
+            }
+        }
+    }
+
 }
+
