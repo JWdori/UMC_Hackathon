29093c634de9bc5be06232f6209c26fc9577fe14
Dori <jaewan0114@naver.com>
2022-07-01 19:08:16 +0900

    병합 conflict 해결

--- app/src/main/java/com/abilitymap/MainActivity.java
+++ app/src/main/java/com/abilitymap/MainActivity.java
@@ -1,15 +1,17 @@
 package com.abilitymap;
-
 import androidx.annotation.NonNull;
 import androidx.appcompat.app.AppCompatActivity;
 import androidx.core.app.ActivityCompat;
 import androidx.fragment.app.FragmentManager;
-
+import android.util.Log;
+import android.widget.TextView;
+import com.naver.maps.map.MapFragment;
+import com.naver.maps.map.NaverMap;
+import com.naver.maps.map.OnMapReadyCallback;
 import android.Manifest;
 import android.content.pm.PackageManager;
 import android.os.Bundle;
 import android.widget.Toast;
-
 import com.naver.maps.geometry.LatLng;
 import com.naver.maps.map.LocationTrackingMode;
 import com.naver.maps.map.MapFragment;
@@ -23,30 +25,23 @@ import java.util.List;
 public class MainActivity extends AppCompatActivity implements OnMapReadyCallback {
     private NaverMap naverMap;
     private FusedLocationSource locationSource;
+
+
     private static final int PERMISSIONS_REQUEST_CODE = 100;
     private static final String[] PERMISSIONS = {
             Manifest.permission.ACCESS_FINE_LOCATION,
             Manifest.permission.ACCESS_COARSE_LOCATION
     };
 
+
     private static final int LOCATION_PERMISSION_REQUEST_CODE = 1000;
     String[] REQUIRED_PERMISSIONS = {
             Manifest.permission.ACCESS_FINE_LOCATION,
             Manifest.permission.ACCESS_COARSE_LOCATION
     };
+
+
     List<LatLng> latLngList = new ArrayList<>();
-
-
-=======
-import android.util.Log;
-import android.widget.TextView;
-
-import com.naver.maps.map.MapFragment;
-import com.naver.maps.map.NaverMap;
-import com.naver.maps.map.OnMapReadyCallback;
-
-public class MainActivity extends AppCompatActivity implements OnMapReadyCallback {
-    TextView tvText;
 
     @Override
     protected void onCreate(Bundle savedInstanceState){ //화면 생성과 함께 현재 위치 받아옴.
@@ -57,7 +52,7 @@ public class MainActivity extends AppCompatActivity implements OnMapReadyCallbac
         MapFragment mapFragment = (MapFragment)fm.findFragmentById(R.id.map);
 
         if(mapFragment ==null ){
-            //mapFragment = MapFragment.newInstance();
+            mapFragment = MapFragment.newInstance();
             fm.beginTransaction().add(R.id.map, mapFragment).commit();
         }
         mapFragment.getMapAsync(this);
@@ -65,14 +60,6 @@ public class MainActivity extends AppCompatActivity implements OnMapReadyCallbac
         locationSource = new FusedLocationSource(this,LOCATION_PERMISSION_REQUEST_CODE);
 
     }
-
-        if(mapFragment == null){
-            mapFragment = MapFragment.newInstance();
-            fm.beginTransaction().add(R.id.map, mapFragment).commit();
-        }
-
-        mapFragment.getMapAsync(this);
-
 
 
 
@@ -83,6 +70,7 @@ public class MainActivity extends AppCompatActivity implements OnMapReadyCallbac
 
         ActivityCompat.requestPermissions(this, PERMISSIONS,
                 PERMISSIONS_REQUEST_CODE); //퍼미션 요청 확인
+        Log.d("MainActivity", "onMapReady");
     }
 
 
@@ -97,11 +85,5 @@ public class MainActivity extends AppCompatActivity implements OnMapReadyCallbac
         }
     }
 
-}
-    @Override
-    public void onMapReady(@NonNull NaverMap naverMap) {
-        Log.d("MainActivity", "onMapReady");
-
-    }
 }
 
